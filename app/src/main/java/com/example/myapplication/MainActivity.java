b045cf42df24551921e340c71f671508f95dee77
v_liangzhiyang@baidu.com <v_liangzhiyang@baidu.com>
2018-10-23 16:07:19 +0800

    add a log

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -46,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+
+        Log.d("tass","hello");
 
         return super.onOptionsItemSelected(item);
     }
