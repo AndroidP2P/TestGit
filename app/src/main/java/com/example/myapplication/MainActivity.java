74494175507dd389d30c72c8c3692cba613dd341
v_liangzhiyang@baidu.com <v_liangzhiyang@baidu.com>
2018-10-23 16:58:04 +0800

    Merge branch 'app_dev'
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -49,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         }
         //add a file here !
         //版本1.4
-
 
         return super.onOptionsItemSelected(item);
     }
